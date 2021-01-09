fded325ff5d41bfd316ba2c70ce58ca79b0566ba
ilanasusual <ilhanmertalan@gmail.com>
2021-01-09 03:27:38 +0300

    c# ile bigquery baglantisini kurma ve basic sql sorgusu

---
+++ BigDataProject_/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BigDataProject_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtboxQuery = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnQueryTitle = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 72);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1019, 561);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtboxQuery
+            // 
+            this.txtboxQuery.Location = new System.Drawing.Point(46, 21);
+            this.txtboxQuery.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxQuery.Name = "txtboxQuery";
+            this.txtboxQuery.Size = new System.Drawing.Size(167, 22);
+            this.txtboxQuery.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(221, 18);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(99, 28);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Get";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(936, 14);
+            this.btnQuery.Margin = new System.Windows.Forms.Padding(4);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(100, 31);
+            this.btnQuery.TabIndex = 6;
+            this.btnQuery.Text = "Get";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(679, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Genres:";
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(740, 20);
+            this.txtQuery.Margin = new System.Windows.Forms.Padding(4);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(185, 22);
+            this.txtQuery.TabIndex = 4;
+            // 
+            // btnQueryTitle
+            // 
+            this.btnQueryTitle.Location = new System.Drawing.Point(571, 17);
+            this.btnQueryTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnQueryTitle.Name = "btnQueryTitle";
+            this.btnQueryTitle.Size = new System.Drawing.Size(100, 28);
+            this.btnQueryTitle.TabIndex = 9;
+            this.btnQueryTitle.Text = "Get";
+            this.btnQueryTitle.UseVisualStyleBackColor = true;
+            this.btnQueryTitle.Click += new System.EventHandler(this.btnQueryTitle_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(386, 21);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(173, 22);
+            this.txtTitle.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 654);
+            this.Controls.Add(this.btnQueryTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxQuery);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "BigQuery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtboxQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnQueryTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTitle;
+    }
+}
+
